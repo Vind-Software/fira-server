784660547ebc2b9d8ea260be7e1f72806279d745
Guilherme Muller <guilherme_mmuller@outlook.com>
2022-07-05 22:16:21 -0300

    Creates nginx container and configure a reverse proxy structure.
      ## Description
      A dockerfile was generated for the nginx container, alongside with the
      configuration file for a virtualhost. The docker compose was updated
      with the new service and the default network received a name
      configuration.
    
      On the application startup routine, a new middleware was added, the
      Microsofts FowardedHeaders. This middleware restores the headers
      schema that was altered by the reverse proxy to be used by the
      application normaly, as if the request was handled by kestrel. Along
      with this change, the https redirect middleware was disabled on the
      dev environment. This will probably be disabled on production too,
      since the use of a reverse proxy, but it is enough for now.

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
+using System.Net;
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.Configure<ForwardedHeadersOptions>(options => {
+    options.KnownProxies.Add(Dns.GetHostEntry("reverse-proxy").AddressList[0] );
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+ForwardedHeadersOptions forwardedHeadersOptions = new ForwardedHeadersOptions {
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+};
+app.UseForwardedHeaders(forwardedHeadersOptions);
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
+} else {
+    app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
