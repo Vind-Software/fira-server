1351d6325db3707e72573e109c43998c391ed2b5
Guilherme Muller <guilherme_mmuller@outlook.com>
2022-09-27 23:55:59 -0300

    Adds a default institution to db install script
    - Adds on the database installation script the insert of the default
      institution of the application.
    - Corrects the naming of the ownerName column, that was owner_name
      before.

--- infra/DB/Relational/Scripts/sc-00.00.0002.sql
+++ infra/DB/Relational/Scripts/sc-00.00.0002.sql
@@ -5,8 +5,10 @@ COMMENT ON SCHEMA service IS 'Holds utilitary tables that dont map directly to d
 CREATE TABLE IF NOT EXISTS service.institutions (
     id SERIAL NOT NULL,
     name character varying(150) NOT NULL,
-    ownerName character varying(150) DEFAULT 'Not Informed',
+    "ownerName" character varying(150) DEFAULT 'Not Informed',
     CONSTRAINT institutions_pkey PRIMARY KEY (id)
 );
 
 COMMENT ON TABLE service.institutions IS 'Holds information about the entity ''Institution''';
+
+INSERT INTO service.institutions(id, name, "ownerName") VALUES (0, 'FIRA Server Default', 'Vind Software');
