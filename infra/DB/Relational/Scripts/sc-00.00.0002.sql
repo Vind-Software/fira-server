3ab4617979fea68e30ae17882a6f426bbe22eaab
Guilherme Muller <guilherme_mmuller@outlook.com>
2022-09-27 23:21:59 -0300

    Creates the institutions table and service schema
    - Creates the "service" schema to hold tables that are not directly
      related to business domain entities.
    - Creates the institutions table on the service schema.
    - Adds a comment to the application schema on the baseline script
    - Creates schema change files for the database operations described
      above.

---
+++ infra/DB/Relational/Scripts/sc-00.00.0002.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA IF NOT EXISTS service;
+
+COMMENT ON SCHEMA service IS 'Holds utilitary tables that dont map directly to domain entities';
+
+CREATE TABLE IF NOT EXISTS service.institutions (
+    id SERIAL NOT NULL,
+    name character varying(150) NOT NULL,
+    ownerName character varying(150) DEFAULT 'Not Informed',
+    CONSTRAINT institutions_pkey PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE service.institutions IS 'Holds information about the entity ''Institution''';
